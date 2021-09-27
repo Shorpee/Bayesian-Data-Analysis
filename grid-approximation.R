885ad20fb168148e57245f1cd99d9cce22c8df89
shorpee <mosopewilliamson@gmail.com>
2021-09-27 17:29:30 +0200

    code for grid approximation

---
+++ grid-approximation.R
@@ -0,0 +1,5 @@
+p_grid <- seq(from =0 , to=1, length.out=1000)
+#prob_p <- rep( 1, 1000 )
+#prob_data <- dbinom(6 , size=9 , prob =p_grid)
+#posterior <- prob_data * prob_p
+#posterior <- posterior / sum(posterior)
