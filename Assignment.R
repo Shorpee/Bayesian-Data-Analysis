7d8ce43bb0ef27a1017d1fa823db7dfccd10b126
shorpee <mosopewilliamson@gmail.com>
2021-10-13 16:34:57 +0200

    add comments and assigned cleaned data to new variable

--- Assignment.R
+++ Assignment.R
@@ -1,9 +1,22 @@
 library(janitor)
 library(dplyr)
+library(tidyverse)
+
 #importing flat file as and reading it as a table
-data <- read.table("https://raw.githubusercontent.com/torkar/dat321/master/data_autumn2020.csv", header = TRUE, sep=";", stringsAsFactors = FALSE)
+data <- read.csv("https://raw.githubusercontent.com/torkar/dat321/master/data_autumn2020.csv", sep=";", stringsAsFactors = FALSE)
+
+#assign data to another variable to be cleaned
 clean <- clean_names(data)
 colnames(clean)
+
+#use tabyl function to check each column and content
 tabyl(clean, technique)
-tabyl(clean, subject)
-tabyl(clean, category)
+
+#use gsub function to replace 0T with OT
+clean <- data.frame(lapply(data, function(x) {
+  gsub("0T", "OT", x)
+}))
+tabyl(clean, technique)
+
+#assign the cleaned data to another variable
+d <- clean
