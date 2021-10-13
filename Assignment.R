e37a97355ade12e8ebd3ec3249372998902d32c7
shorpee <mosopewilliamson@gmail.com>
2021-10-13 15:30:47 +0200

    cleaning data with tidyverse

--- Assignment.R
+++ Assignment.R
@@ -1,1 +1,9 @@
-data <- read.csv("https://raw.githubusercontent.com/torkar/dat321/master/data_autumn2020.csv", sep=";")
+library(janitor)
+library(dplyr)
+#importing flat file as and reading it as a table
+data <- read.table("https://raw.githubusercontent.com/torkar/dat321/master/data_autumn2020.csv", header = TRUE, sep=";", stringsAsFactors = FALSE)
+clean <- clean_names(data)
+colnames(clean)
+tabyl(clean, technique)
+tabyl(clean, subject)
+tabyl(clean, category)
